7986a5cf8c603cc742daa5733426df7b1cfe3a56
PipooPenguin <21522599@gm.uit.edu.vn>
2024-10-12 01:49:36 +0700

    create route table and nat gateway aws with terraform

--- terraform_lab1/modules/VPC/main.tf
+++ terraform_lab1/modules/VPC/main.tf
@@ -20,3 +20,63 @@ resource "aws_subnet" "public_subnet" {
         Name = "public_subnet"
     }
 }
+
+resource "aws_internet_gateway" "vpc_internetGateway" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "vpc_internetGateway"
+  }
+}
+
+resource "aws_route_table" "public_routeTable" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.vpc_internetGateway.id
+  }
+
+  route {
+    ipv6_cidr_block = "::/0"
+    gateway_id = aws_internet_gateway.vpc_internetGateway.id
+  }
+
+  tags = {
+    Name = "public_routeTable"
+  }
+}
+
+resource "aws_route_table_association" "association_public" {
+  subnet_id      = aws_subnet.public_subnet.id
+  route_table_id = aws_route_table.public_routeTable.id
+}
+
+resource "aws_eip" "nat_eip" {
+    vpc = true
+}
+resource "aws_nat_gateway" "natGateway"{
+    allocation_id = aws_eip.nat_eip.id 
+    subnet_id= aws_subnet.public_subnet.id
+    tags = {
+        Name = "NAT"
+    }
+}
+
+# define private subnet will all go to nat gateway
+resource "aws_route_table" "private_routeTable" {
+    vpc_id = aws_vpc.main.id
+    route {
+        cidr_block = "0.0.0.0/0"
+        gateway_id = aws_nat_gateway.natGateway.id
+    }    
+    tags = {
+        Name = "private_routeTable"
+    }
+
+}
+
+resource "aws_route_table_association" "association_private" {
+  subnet_id      = aws_subnet.private_subnet.id
+  route_table_id = aws_route_table.private_routeTable.id
+}
